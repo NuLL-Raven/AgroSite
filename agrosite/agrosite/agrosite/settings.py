4d45d05e9b8f13091cca1079964d50299c703ca2
Adel-oTh <adelothmani2005@gmail.com>
2025-10-02 01:47:39 +0200

    Add whitenoise dependency

--- agrosite/agrosite/agrosite/settings.py
+++ agrosite/agrosite/agrosite/settings.py
@@ -132,8 +132,6 @@ LOCALE_PATHS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
 

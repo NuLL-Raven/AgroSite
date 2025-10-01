ef232576e0e6991dc3718c85566403afd2d296ac
Adel-oTh <adelothmani2005@gmail.com>
2025-10-02 01:37:45 +0200

    Add whitenoise dependency

--- agrosite/products/models.py
+++ agrosite/products/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/')
 
     def __str__(self):
         return self.name

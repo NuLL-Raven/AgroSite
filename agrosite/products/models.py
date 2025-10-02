79d8950914229255e7c4599a5574531e1eb21335
Adel-oTh <adelothmani2005@gmail.com>
2025-10-02 02:27:10 +0200

    Add cloudinary to requirements

--- agrosite/products/models.py
+++ agrosite/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField()
     image = models.ImageField(
         upload_to='product_images/',

c65f8146cb53f89343224f21952d1336aa6b25cb
Adel-oTh <adelothmani2005@gmail.com>
2025-10-02 02:05:20 +0200

    Add cloudinary to requirements

--- agrosite/agrosite/settings.py
+++ agrosite/agrosite/settings.py
@@ -134,13 +134,16 @@ LOCALE_PATHS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 
 
 JAZZMIN_SETTINGS = {

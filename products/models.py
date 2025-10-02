673575e7f786f018ba9ddb22df42fe3874474a5c
Adel-oTh <adelothmani2005@gmail.com>
2025-10-02 02:13:33 +0200

    Add cloudinary to requirements

--- products/models.py
+++ products/models.py
@@ -1,11 +1,14 @@
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', default='default-image.jpg')
-
+    image = models.ImageField(
+        upload_to='product_images/',
+        storage=MediaCloudinaryStorage()  # <- explicitly use Cloudinary
+    )
 
     def __str__(self):
         return self.name
